21625094a286c3f6ba28916d62d8c2dcb311a15f
Osothetechnerd <oifidon@albany.edu>
2026-09-02 14:38:41 -0400

    Initial Commit

---
+++ Assets/Button Click.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public TextMeshProUGUI textbox;
+    public void ButtonClickk()
+    {
+
+        textbox.text = "I have changed";
+    }
+
+}
